26eb697475811b24e9e8fd37d0873c513cd6363a
LoganLieou <logan.lieou@gmail.com>
2023-09-18 18:22:36 -0500

    Initial Commit

---
+++ task manager/task_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  task_managerApp.swift
+//  task manager
+//
+//  Created by Logan  Jackson on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct task_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
